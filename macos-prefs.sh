dda5a67ac3bc0a3dbcb866292f2462e89be0355e
abdulrohim.sama <abdulrohim.sama@earnin.com>
2025-09-08 22:47:48 +0700

    update

--- macos-prefs.sh
+++ macos-prefs.sh
@@ -10,7 +10,7 @@ echo "⚓ Configuring Dock..."
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0
-defaults write com.apple.dock show-recents -bool false
+defaults write com.apple.dock show-recents -bool true
 defaults write com.apple.dock tilesize -int 48
 defaults write com.apple.dock magnification -bool true
 defaults write com.apple.dock largesize -int 64
